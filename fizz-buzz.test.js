ad9562a096acb4a5bea3eceb6a4b45a3a6364484
Jake Corn <32077083+JakeTheCorn@users.noreply.github.com>
2025-05-16 08:59:49 -0400

    Add directions for Tdd

--- fizz-buzz.test.js
+++ fizz-buzz.test.js
@@ -1,8 +1,19 @@
-import { describe, it } from "vitest";
-
+import { describe, it, expect } from "vitest";
+import { a } from './fizz-buzz';
 
 describe("fizzBuzz", () => {
   it("does something", () => {
-    
+    expect(true).toEqual(false);
   });
 });
+
+
+/**
+ * Test Driven Development
+ * 
+ * 0.) Write a todo in the todo list - this keeps our mind clear while working
+ * 
+ * 1.) Red - Write a failing test - before any production code can be written
+ * 2.) Green - make the test pass by implementing the functionality - do as fast as possible
+ * 3.) Refactor - improve the design of the code while keeping the test passing
+ */
--- fizz-buzz.ts
+++ fizz-buzz.ts
@@ -1,1 +1,4 @@
 // Put implementation here
+export function fizzBuzz() {
+  
+}
